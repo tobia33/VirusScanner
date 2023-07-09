29b975e737b8308129cfc553abf18105e018d7ff
ValerioBaldi <baldi.1940729@studenti.uniroma1.it>
2023-07-09 19:11:13 +0200

    Aggiunti test su sign up

--- web_application/spec/system/sign_up_spec.rb
+++ web_application/spec/system/sign_up_spec.rb
@@ -2,10 +2,13 @@ require "rails_helper"
 
 describe "User signs up", type: :system do
   let(:username) { Faker::Internet.username }
+  let(:usernamegiainuso) {Faker::Internet.username }
   let(:email) {Faker::Internet.email}
   let(:password) { Faker::Internet.password(min_length: 8) }
+  let(:wrong_password) {Faker::Internet.password}
 
   before do
+    WebMock.disable_net_connect!(:allow_localhost => true)
     visit new_user_registration_path
   end
 
@@ -17,5 +20,29 @@ describe "User signs up", type: :system do
     click_button "Sign up"
 
     expect(page).to have_text "You need to sign in or sign up before continuing."
+  end
+
+  scenario "with invalid data, different passwords" do
+    fill_in "user_username", with: username
+    fill_in "user_email", with: email
+    fill_in "user_password", with: wrong_password
+    fill_in "user_password_confirmation", with: password
+    click_button "Sign up"
+
+    expect(page).to have_text "Error, email already exists or some fields aren't filled correctly"
+  end
+
+  before do
+    @user = User.create(username: usernamegiainuso, email: 'emaildiesempio@mail.com', password: 'passwordpocosicura')
+  end
+
+  scenario "with invalid data, username already in use" do
+    fill_in "user_username", with: usernamegiainuso
+    fill_in "user_email", with: email
+    fill_in "user_password", with: wrong_password
+    fill_in "user_password_confirmation", with: password
+    click_button "Sign up"
+
+    expect(page).to have_text "Username already exist"
   end
 end
